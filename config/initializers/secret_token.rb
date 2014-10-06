b0ed5ffb16b63639efa19dbb02ac58997220676e
Jamie Cheng <jacheng@vt.edu>
2014-10-06 00:00:29 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineReview::Application.config.secret_key_base = '75b863f500180c469dd89740d3da05b01b334fb0a79992cef34d7ebd3db8d8d7d8498b43aa037055f5b34ab9dba54ac9863c81f213675895c51748b043f3829e'
