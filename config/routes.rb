63941f79b872f1127145318ed47a2b433f8b2ba7
Jamie Cheng <jacheng@vt.edu>
2014-10-08 00:14:15 -0400

    hw class 3 lab 6 partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 WineReview::Application.routes.draw do
-  get "/wines" => "wines#index", as: 'wines'
-  get "/wines/new" => "wines#new", as: 'new_wine'  
-  get "/wines/:id" => "wines#show", as: 'wine'
-  post "/wines" => "wines#create"
-  get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
-  patch "/wines/:id" => "wines#update"
-  delete "wines/:id" => "wines#destroy"
+ resources :wines
+
+ # Previous routes, replaced by resources
+ # get "/wines" => "wines#index", as: 'wines'
+ # get "/wines/new" => "wines#new", as: 'new_wine'  
+ # get "/wines/:id" => "wines#show", as: 'wine'
+ # post "/wines" => "wines#create"
+ # get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+ # patch "/wines/:id" => "wines#update"
+ # delete "wines/:id" => "wines#destroy"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
