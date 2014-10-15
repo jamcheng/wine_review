7d6e35d15df4bc8554e3f5395b8f6f75a2124402
Jamie Cheng <jacheng@vt.edu>
2014-10-14 21:08:53 -0400

    hw 4 lab 5 complete

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,7 +1,8 @@
 class Wine < ActiveRecord::Base
+	VARIETALS = ['Chianti', 'Syrah', 'Cabernet Sauvignon', 'Malbec', 'Riesling', 'Shiraz', 'Merlot','Champagne']
 	validates :name, :year, :country, presence: true
 	validates :year, 
 		numericality: { only_integer: true }, 
 		unless: "year.blank?"
-	validates :varietal, inclusion: {in: %w(Red White), message: "%{value} is not a valid varietal"}
+	validates :varietal, inclusion: {in: VARIETALS, message: "%{value} is not a valid varietal"}
 end
